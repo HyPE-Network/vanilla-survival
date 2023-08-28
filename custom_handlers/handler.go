0955d494080a7e6e9addd7ca8329d028eab47bd3
Ermolay <es.ermolaev2005@gmail.com>
2023-08-28 22:00:13 +0300

    Fix handler

--- custom_handlers/handler.go
+++ custom_handlers/handler.go
@@ -1,7 +1,7 @@
 package custom_handlers
 
 import (
-	"github.com/HyPE-Network/vanilla-proxy/proxy/block"
+	"github.com/HyPE-Network/vanilla-proxy/proxy/block/cube"
 	"github.com/HyPE-Network/vanilla-proxy/proxy/player/human"
 	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
@@ -17,10 +17,11 @@ func (PlaceBlockHandler) Handle(pk packet.Packet, player human.Human) (bool, pac
 	case *protocol.UseItemTransactionData:
 		if td.ActionType == protocol.UseItemActionClickBlock {
 			if td.HeldItem.Stack.BlockRuntimeID != 0 {
-				state := block.Blocks[td.HeldItem.Stack.BlockRuntimeID]
+				if td.HeldItem.Stack.ItemType.NetworkID == 49 && player.InNether() { // obsidian block in nether
+					pos := cube.Side(td.BlockPosition, td.BlockFace)
 
-				if state.Name == "minecraft:obsidian" && player.InNether() {
 					player.SendMessage("§cYou can't place obsidian in this world!")
+					player.SendAirUpdate(protocol.BlockPos{pos.X(), pos.Y(), pos.Z()})
 					return false, pk, nil
 				}
 			}
